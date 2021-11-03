018475211d2efab8618750cbc6ad3001db5029b1
Jonas Rigter <jonas.rigter@gmail.com>
2021-11-03 21:12:22 +0100

    read excel and csv files, es fehlt wenig (Davide, 03.11)

---
+++ src/main/java/com/rigter/miceli/modifier/CsvRecord.java
@@ -0,0 +1,151 @@
+package com.rigter.miceli.modifier;
+
+import java.util.List;
+
+public class CsvRecord {
+
+    //headers
+    private String Transfer_Code;
+    private String Container_From;
+    private String  Account_From;
+    private String  Currency;
+    private String  Amount;
+    private String  Containter_To;
+    private String  Account_To;
+    private String  Beneficiary_IBAN_Account;
+    private String  Beneficiary_Account;
+    private String  Beneficiary;
+    private String  Destination_Bank;
+    private String  Value_Date;
+    private String  Avis_Text;
+    private String  Month;
+    
+    public CsvRecord (String[] fields) {
+        this.Transfer_Code = fields[0];
+        this.Container_From = fields[1];
+        this.Account_From = fields[2];
+        this.Currency = fields[3];
+        this.Amount = fields[4];
+        this.Containter_To = fields[5];
+        this.Account_To = fields[6];
+        this.Beneficiary_IBAN_Account = fields[7];
+        this.Beneficiary_Account = fields[8];
+        this.Beneficiary = fields[9];
+        this.Destination_Bank = fields[10];
+        this.Value_Date = fields[11];
+        this.Avis_Text = fields[12];
+        this.Month = fields[13];
+    }
+
+    public String getTransfer_Code() {
+        return Transfer_Code;
+    }
+
+    public void setTransfer_Code(String transfer_Code) {
+        Transfer_Code = transfer_Code;
+    }
+
+    public String getContainer_From() {
+        return Container_From;
+    }
+
+    public void setContainer_From(String container_From) {
+        Container_From = container_From;
+    }
+
+    public String getAccount_From() {
+        return Account_From;
+    }
+
+    public void setAccount_From(String account_From) {
+        Account_From = account_From;
+    }
+
+    public String getCurrency() {
+        return Currency;
+    }
+
+    public void setCurrency(String currency) {
+        Currency = currency;
+    }
+
+    public String getAmount() {
+        return Amount;
+    }
+
+    public void setAmount(String amount) {
+        Amount = amount;
+    }
+
+    public String getContainter_To() {
+        return Containter_To;
+    }
+
+    public void setContainter_To(String containter_To) {
+        Containter_To = containter_To;
+    }
+
+    public String getAccount_To() {
+        return Account_To;
+    }
+
+    public void setAccount_To(String account_To) {
+        Account_To = account_To;
+    }
+
+    public String getBeneficiary_IBAN_Account() {
+        return Beneficiary_IBAN_Account;
+    }
+
+    public void setBeneficiary_IBAN_Account(String beneficiary_IBAN_Account) {
+        Beneficiary_IBAN_Account = beneficiary_IBAN_Account;
+    }
+
+    public String getBeneficiary_Account() {
+        return Beneficiary_Account;
+    }
+
+    public void setBeneficiary_Account(String beneficiary_Account) {
+        Beneficiary_Account = beneficiary_Account;
+    }
+
+    public String getBeneficiary() {
+        return Beneficiary;
+    }
+
+    public void setBeneficiary(String beneficiary) {
+        Beneficiary = beneficiary;
+    }
+
+    public String getDestination_Bank() {
+        return Destination_Bank;
+    }
+
+    public void setDestination_Bank(String destination_Bank) {
+        Destination_Bank = destination_Bank;
+    }
+
+    public String getValue_Date() {
+        return Value_Date;
+    }
+
+    public void setValue_Date(String value_Date) {
+        Value_Date = value_Date;
+    }
+
+    public String getAvis_Text() {
+        return Avis_Text;
+    }
+
+    public void setAvis_Text(String avis_Text) {
+        Avis_Text = avis_Text;
+    }
+
+    public String getMonth() {
+        return Month;
+    }
+
+    public void setMonth(String month) {
+        Month = month;
+    }
+}
--- src/main/java/com/rigter/miceli/modifier/MyFileChangeListener.java
+++ src/main/java/com/rigter/miceli/modifier/MyFileChangeListener.java
@@ -1,16 +1,24 @@
 package com.rigter.miceli.modifier;
 
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.boot.devtools.filewatch.ChangedFile;
 import org.springframework.boot.devtools.filewatch.ChangedFiles;
 import org.springframework.boot.devtools.filewatch.FileChangeListener;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
 import java.nio.channels.FileChannel;
 import java.nio.channels.FileLock;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 @Component
@@ -18,30 +26,64 @@ public class MyFileChangeListener implements FileChangeListener {
 
     @Override
     public void onChange(Set<ChangedFiles> changeSet) {
+        ArrayList<CsvRecord> recordsList = new ArrayList<>();
+        String accountNr = "";
         for(ChangedFiles cfiles : changeSet) {
             for(ChangedFile cfile: cfiles.getFiles()) {
-                //TODO implement business logic
                 if(!isLocked(cfile.getFile().toPath())) {
                     try {
+
+                        //TODO file extension check
+                        //********* EXCEL READ ******************
+
+                        XSSFWorkbook workbook = new XSSFWorkbook(cfile.getFile());
+                        XSSFSheet worksheet = workbook.getSheetAt(2);
+                        for(int i=0;i<worksheet.getPhysicalNumberOfRows() ;i++) {
+
+                            XSSFRow row = worksheet.getRow(i);
+
+                            if (row != null && row.getCell(0) != null && row.getCell(0).getStringCellValue().equalsIgnoreCase("Konto-Nummer:")) {
+                                accountNr = row.getCell(1).getStringCellValue();
+                                System.out.println(accountNr);
+                            }
+                        }
+
+
+                        //****** CSV READ ************
                         File file = cfile.getFile();
                         FileReader fr = new FileReader(file);
                         BufferedReader br = new BufferedReader(fr);
                         String line = "";
                         String[] tempArr;
                         while((line = br.readLine()) != null) {
-                            tempArr = line.split(",");
-                            for(String tempStr : tempArr) {
-                                System.out.print(tempStr + " ");
+                            tempArr = line.split(";");  //,,,,,,,,,,,,,,,,,,,,,,,
+                            if (tempArr.length == 0 || tempArr[0].equalsIgnoreCase("Transfer_Code")) {
+                                //this is the header line, do nothing
+                                continue;
                             }
-                            System.out.println();
+                            recordsList.add(new CsvRecord(tempArr));
                         }
+                        System.out.print(recordsList);
                         br.close();
-                    } catch(IOException ioe) {
+                    } catch(IOException | InvalidFormatException ioe) {
                         ioe.printStackTrace();
                     }
+
+
                 }
             }
         }
+
+        // Logik mit überschreiben
+        for (CsvRecord record : recordsList) {
+            if (record.getAccount_From().equalsIgnoreCase(accountNr)) {
+                //
+                record.setAmount("10000000000000000000000");
+                System.out.println("FOUND ACCOUNT!!!!!!!!!!!!!!!!!!!!");
+            }
+        }
+
+
     }
 
     private boolean isLocked(Path path) {
